56a98ca2f020f3371ce0e45939cc20acefba4430
kalasoo <ym.kalasoo@gmail.com>
2026-08-22 21:40:22 +0800

    bump version to 0.5.8

--- VibeUsage/Models/AppConfig.swift
+++ VibeUsage/Models/AppConfig.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 enum AppConfig {
-    static let version = "0.5.7"
+    static let version = "0.5.8"
 
     static let cliIdentityEnvironment = [
         "VIBE_USAGE_SURFACE": "mac-app",
